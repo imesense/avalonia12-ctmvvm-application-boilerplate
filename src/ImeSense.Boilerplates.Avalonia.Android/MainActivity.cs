0d39c6903bd619d44dcd159d5a0a084a194365d8
acidicMercury8 <acidicmercury8@outlook.com>
2026-05-18 22:38:23 +0300

    Add Android project
    Implements GitHub issue #6

---
+++ src/ImeSense.Boilerplates.Avalonia.Android/MainActivity.cs
@@ -0,0 +1,20 @@
+using Android.App;
+using Android.Content.PM;
+
+using Avalonia.Android;
+
+namespace ImeSense.Boilerplates.Avalonia.Android;
+
+[Activity(
+    Label = "ImeSense.Boilerplates.Avalonia.Android",
+    Theme = "@style/MyTheme.NoActionBar",
+    Icon = "@drawable/icon",
+    MainLauncher = true,
+    ConfigurationChanges =
+        ConfigChanges.Orientation |
+        ConfigChanges.ScreenSize |
+        ConfigChanges.UiMode
+)]
+public class MainActivity : AvaloniaMainActivity
+{
+}
